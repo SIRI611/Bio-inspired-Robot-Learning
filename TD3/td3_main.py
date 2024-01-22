1f495ed1092dbb9cab22b67b71feb2829d6d5c1f
zhuhy <zhuhy57@mail2.sysu.edu.cn>
2024-01-22 19:18:08 +0800

    modified:   TD3/td3_main.py Changed to Tianqi's idea

--- TD3/td3_main.py
+++ TD3/td3_main.py
@@ -24,6 +24,8 @@ class Config():
         self.is_train = False
         self.is_continue_train = True
         self.continue_train_episodes = 3000
+        self.average_a = 0.9        # 10
+        self.average_b = 0.98       # 50
         self.lr = 1e-3
         # self.modelfilepath = "td3_humanoid.pkl"
         self.env = "Ant-v4"
@@ -49,8 +51,8 @@ def calculate_amp_init(gradient_path, weight_path, k1, k2):
 
 para = Config()
 episode_rewards = list()
-# env = gym.make(para.env, render_mode="human")
 env = gym.make(para.env, render_mode="human")
+# env = gym.make(para.env)
 
 
 if para.is_train:
@@ -68,6 +70,9 @@ if para.is_train:
 else:
     model = TD3.load("save_model/{}_{}.pkl".format(para.env_name, para.total_step))
     reward_list = deque()
+    reward_average = 0
+    reward_diff = 0
+    reward_diff_average = 0     #alpha
     if not para.is_continue_train:
         for _ in range(para.num_test):    
             state = env.reset()[0]
@@ -86,7 +91,9 @@ else:
 
     if para.is_continue_train:
         amp_init = calculate_amp_init(para.gradient_path, para.weight_path, para.k1, para.k2)
-        model.actor.optimizer_dynamic = DynamicSynapse(model.actor.parameters(), lr=para.lr, amp=amp_init, period=4000, dt=para.dt)
+        model.actor.optimizer_dynamic = DynamicSynapse(model.actor.parameters(), lr=para.lr, amp=amp_init, period=4000, dt=para.dt,
+                                                       a=1e-3,
+                                                       b=-1e-3)
         for episode_idx in range(para.continue_train_episodes):
             state = env.reset()[0]
             episode_reward = 0
@@ -98,19 +105,30 @@ else:
                 state, reward, done, _, _ = env.step(action)
                 # print(done)
 
-                if len(reward_list) > 0:
-                    sum_ = sum(reward_list)
-                    reward_ = reward - (sum_/len(reward_list))
-                    reward_list.append(reward)
-                    if len(reward_list) > 50:
-                        reward_list.popleft()
-                        
-                if len(reward_list) == 0:
-                    reward_ = reward
-                    reward_list.append(reward)
-                print(reward_)
+                if reward_average == 0:
+                    reward_average = reward
+                else:
+                    reward_average = para.average_a * reward_average + (1 - para.average_a) * reward
+                    reward_diff = reward - reward_average
+                
+                reward_diff_average = para.average_b * reward_diff_average + (1 - para.average_b) * reward_diff
+                # print(reward_average)
+                # print(reward_diff_average)
 
-                model.actor.learn_dynamic(reward_)
+                # if len(reward_list) > 0:
+                #     sum_ = sum(reward_list)
+                #     reward_ = reward - (sum_/len(reward_list))
+                #     reward_list.append(reward)
+                #     if len(reward_list) > 50:
+                #         reward_list.popleft()
+                        
+                # if len(reward_list) == 0:
+                #     reward_ = reward
+                #     reward_list.append(reward)
+                # print(reward_)
+
+                # model.actor.learn_dynamic(reward_)
+                model.actor.learn_dynamic(reward_diff_average)
                 episode_reward += reward
                 
                 # if step == 10000:
@@ -120,8 +138,18 @@ else:
 
                 if done:
                     # print("break")
-                    break 
+                    break
+            
+            print("oscillate weight center:")
+            print(model.actor.optimizer_dynamic.state_dict()['state'][5]['weight_centre'])
+            print("weight_oscillate_decay:")
+            print(model.actor.optimizer_dynamic.state_dict()['state'][5]['weight_oscilate_decay'])
+            print("oscillate amp:")
+            print(model.actor.optimizer_dynamic.state_dict()['state'][5]['amp'])
+            print("episode:", episode_idx, "\nepisode_reward:", episode_reward, "\n")  
             episode_rewards.append(episode_reward)
+        model.save("save_model/continue_train_{}_{}.pkl".format(para.env_name, para.continue_train_episodes))
+# with open("trace.pkl", "rb") as f:
 # with open("trace.pkl", "rb") as f:
 #     trace = dill.load(f)
 # print(trace["weight"])
