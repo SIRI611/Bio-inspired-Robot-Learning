4050a18743fd57e6f80bedbe58fe606d36f5b076
zhuhy <zhuhy57@mail2.sysu.edu.cn>
2024-03-13 01:14:23 +0800

    modified:   predict_train_cnn.py

--- predict_train_cnn.py
+++ predict_train_cnn.py
@@ -18,7 +18,7 @@ class Config():
     env = "Humanoid-v4"
     env_name = "humanoid_tqc"
     total_step=2e6
-    num_test = 5000
+    num_test = 1000
     if_train = True
     step_num = 20
     batch_size = 32
@@ -33,20 +33,22 @@ model = TQC.load("save_model/{}_{}.pkl".format(para.env_name, para.total_step))
 env = gym.make(para.env)
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.shape[0]
-cluster = deque(maxlen=para.step_num)
 fall_step_offset = (1 + para.step_num) / 2
+
 predict_net = Predict(batch_size=para.batch_size, device=para.device)
 
 
 
 if para.if_train:
-    num = int(1e6)
-    replay_buffer_len = int(1e6)
+    num = int(5e5)
+    replay_buffer_len = int(5e5)
     replay_buffer_list = [i for i in range(replay_buffer_len)]
     replay_buffer = deque(maxlen=replay_buffer_len)
     fall_step = deque(maxlen=replay_buffer_len)
+    cluster = deque(maxlen=para.step_num)
     step_temp = deque()
-    loss_list = list()
+
+    loss_list = deque()
 
     action_list = deque(maxlen=num)
     state_list = deque(maxlen=num)
@@ -97,7 +99,6 @@ if para.if_train:
                             sample = np.random.choice([i for i in range(len(replay_buffer))], size=para.batch_size, replace=False)
                         else:
                             sample = np.random.choice(replay_buffer_list, size=para.batch_size, replace=False)
-                        inputs = []
                         # print(replay_buffer[sample].shape)
                         inputs = np.expand_dims(np.array([replay_buffer[j] for j in sample]), axis=1)
                         target = np.array([f2(fall_step[k]) for k in sample]).reshape((para.batch_size, -1))
@@ -116,40 +117,52 @@ if para.if_train:
             
         # print("final reward = ", np.mean(episode_rewards), "±", np.std(episode_rewards))
 else:
+    num = int(1e5)
     para.num_test = 10
     predict_net.load_state_dict(torch.load("save_predict/" + "humanoid_tqc_2000000.0_5000_0306_032220_50" + ".pkl"))
     fall_step_predict = deque()
     fall_step_total = deque()
+    state_action_actor = deque(maxlen=num)
+    cluster = deque(maxlen=para.step_num)
     total_step = 0
     for i in range(para.num_test):
         state = env.reset()[0]
+        cluster.clear()
+        state_action_actor.clear()
         step = 0
         while(True):
             total_step += 1
             step += 1
             action, _state = model.predict(state, deterministic=True)
             state, reward, done, _, _ = env.step(action)
-            predict_step = predict_net(np.concatenate((state, action))).detach().numpy()[0]
-            fall_step_predict.append(predict_step)
+            state_action = np.concatenate((state, action, np.array([0])))
+            cluster.append(state_action)
+            if len(cluster) == para.step_num:
+                state_action_actor.append(cluster)
+                # predict_step = predict_net(np.expand_dims(np.array(cluster), axis=0)).detach().numpy()[0]
+                # fall_step_predict.append(predict_step)
             # fall_step_total.append(predict_step)
             # print(fall_step_predict)
             if done:
                 print(step)
+                print(np.array(state_action_actor).shape)
+
+                predict_step = predict_net(np.expand_dims(np.array(state_action_actor), axis=1))
                 window_length = 200  # 窗口长度
                 polyorder = 2      # 多项式阶数
-                y_smoothed = savgol_filter(fall_step_predict, window_length, polyorder)
-                plt.plot([x for x in range(step)], fall_step_predict, linewidth=0.6, label="origin")
-                plt.plot([x for x in range(step)], y_smoothed, linewidth=0.8, label="smooth")
+                y_smoothed = savgol_filter(predict_step, window_length, polyorder)
+                plt.plot([x for x in range(len(state_action_actor))], predict_step, linewidth=0.6, label="origin")
+                plt.plot([x for x in range(len(state_action_actor))], y_smoothed, linewidth=0.8, label="smooth")
                 plt.axhline(0, linewidth=0.5, color='black')
                 plt.axhline(0.5, linewidth=0.5, color='black')
                 plt.legend()
                 plt.show()
                 # os.system("pause")
-                fall_step_total.extend(fall_step_predict)
-                fall_step_predict.clear()
+                # fall_step_total.extend(fall_step_predict)
+                # fall_step_predict.clear()
                 break
         
-    fig = plt.figure(figsize=(18, 5))
-    plt.plot([y for y in range(int(total_step))], fall_step_total, linewidth=0.4)
-    plt.show()
+    # fig = plt.figure(figsize=(18, 5))
+    # plt.plot([y for y in range(int(total_step))], fall_step_total, linewidth=0.4)
+    # plt.show()
         
